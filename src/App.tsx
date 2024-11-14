d30bdde9b4c0b1c0cebc13b0943ee63084463710
maurolonmartin <maurolonmartin@hotmail.com>
2024-11-14 14:56:30 -0500

    useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
-import { useState } from 'react'
-import { Button } from './components'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Character {
+  id: number,
+  name: string,
+  image: string,
+  status: string
+}
 
-  const countMore = () => {
-    setCount((count) => count + 1)
+function App() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+const fecthData = async () => {
+  setLoading(true)
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character")
+
+    if(!response.ok) {
+      throw new Error("Error al obtener datos")
+    }
+
+    const jsonData = await response.json()
+    const jsonDataResults = await jsonData.results
+    setData(jsonDataResults)
+  } catch(err) {
+    setError(err as string)
+  } finally {
+    setLoading(false)
   }
+} 
+
+useEffect(() => {
+  fecthData()
+}, [])
+  
+if(loading){
+  return <div>Cargando...</div>
+}
+
+if(error){
+  return <div>UPS! hay un error: {error}</div>
+}
 
   return (
     <>
-      <Button label={`Count is ${count}`} parentMethod={countMore} ></Button>
+      {data.map((item:Character) => (
+        <div className='card' key={item.id}>
+          <img src={item.image} alt={item.name} />
+          <p>{item.name}</p>
+          <span>{item.status}</span>
+        </div>
+      ))}
     </>
+  
   )
 }
 
